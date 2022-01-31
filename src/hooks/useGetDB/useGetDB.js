b3da1b85782f6f058f028479ac604a9ab9e4153d
Juan Barrios <juanbarrios045@gmail.com>
2022-01-31 09:59:37 -0400

    fix text

--- src/hooks/useGetDB/useGetDB.js
+++ src/hooks/useGetDB/useGetDB.js
@@ -16,12 +16,16 @@ const useGetDB = () => {
   const dispatch = useDispatch()
 
   const getNominateds = async () => {
-    console.log("Test re renders hook")
-    const dataN = await getCollectionsFirebase("nominateds")
-    const dataC = await getCollectionsFirebase("categories")
+    try {
+      const dataN = await getCollectionsFirebase("nominateds")
+      const dataC = await getCollectionsFirebase("categories")
 
-    dispatch(setNominateds(dataN))
-    dispatch(setCategories(dataC))
+      dispatch(setNominateds(dataN))
+      dispatch(setCategories(dataC))
+    } catch (e) {
+      dispatch(setNominateds([]))
+      dispatch(setCategories([]))
+    }
   }
 
   useEffect(() => {
