a88b0fbea0f7672e997e790ccf400ec867399fbe
Carlos Barrios <juanbarrios045@gmail.com>
2023-01-26 02:26:18 -0400

    Master | Clean dependencies

--- src/components/Organisms/LoginForm/LoginForm.jsx
+++ src/components/Organisms/LoginForm/LoginForm.jsx
@@ -1,89 +1,89 @@
-import { useFormik } from "formik"
-import { useSupabaseClient } from "@supabase/auth-helpers-react"
-// import Router from "next/router"
+// import { useFormik } from "formik"
+// import { useSupabaseClient } from "@supabase/auth-helpers-react"
+// // import Router from "next/router"
 
-// Layout
-import { Heading, Button, Card, Box, Text, useToast } from "@chakra-ui/react"
-import { Input } from "src/components/Atoms"
+// // Layout
+// import { Heading, Button, Card, Box, Text, useToast } from "@chakra-ui/react"
+// import { Input } from "src/components/Atoms"
 
-// Validations
-import useValidations from "src/hooks/useValidations"
-import useValidationsInput from "src/hooks/useValidationsInput"
+// // Validations
+// import useValidations from "src/hooks/useValidations"
+// import useValidationsInput from "src/hooks/useValidationsInput"
 
-const LoginForm = () => {
-  const toast = useToast()
+// const LoginForm = () => {
+//   const toast = useToast()
 
-  const { SignInSchema } = useValidations()
-  const { funcIsError, funcIsTextError } = useValidationsInput()
-  const supabase = useSupabaseClient()
+//   const { SignInSchema } = useValidations()
+//   const { funcIsError, funcIsTextError } = useValidationsInput()
+//   const supabase = useSupabaseClient()
 
-  // Func SignUp
-  async function signInWithEmail() {
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: "example@email.com",
-      password: "example-password",
-    })
-    if (error) {
-      toast({
-        title: "Error",
-        description: error?.message,
-        status: "error",
-        duration: 9000,
-        isClosable: true,
-      })
+//   // Func SignUp
+//   async function signInWithEmail() {
+//     const { data, error } = await supabase.auth.signInWithPassword({
+//       email: "example@email.com",
+//       password: "example-password",
+//     })
+//     if (error) {
+//       toast({
+//         title: "Error",
+//         description: error?.message,
+//         status: "error",
+//         duration: 9000,
+//         isClosable: true,
+//       })
 
-      console.error("Sign In ->", error)
-      return
-    }
-    console.log(data, error)
+//       console.error("Sign In ->", error)
+//       return
+//     }
+//     console.log(data, error)
 
-    // Router.push("/")
-  }
-  const handleUser = (data) => console.log(data)
+//     // Router.push("/")
+//   }
+//   const handleUser = (data) => console.log(data)
 
-  const { handleSubmit, errors, values, handleChange, touched, setErrors } =
-    useFormik({
-      initialValues: { email: "", password: "" },
-      onSubmit: signInWithEmail,
-      validationSchema: SignInSchema,
-    })
+//   const { handleSubmit, errors, values, handleChange, touched, setErrors } =
+//     useFormik({
+//       initialValues: { email: "", password: "" },
+//       onSubmit: signInWithEmail,
+//       validationSchema: SignInSchema,
+//     })
 
-  return (
-    <Card p="40px" w="100%">
-      <Heading as="h1" size="md" textAlign="center">
-        Iniciar sesión
-      </Heading>
+//   return (
+//     <Card p="40px" w="100%">
+//       <Heading as="h1" size="md" textAlign="center">
+//         Iniciar sesión
+//       </Heading>
 
-      <Box as="form" onSubmit={handleSubmit} p="20px">
-        <Input
-          name="email"
-          placeholder="Correo electrónico"
-          value={values.email}
-          onChange={handleChange}
-          // error={funcIsError(errors.email || errors.password, touched.email)}
-          // helperText={funcIsTextError(errors.email, touched.email)}
-          mb="20px"
-        />
-        <Input
-          name="password"
-          type="password"
-          placeholder="Contraseña"
-          value={values.password}
-          onChange={handleChange}
-          // error={funcIsError(errors.password, touched.password)}
-          // helperText={funcIsTextError(errors.password, touched.password)}
-          mb="20px"
-        />
-        <Button type="submit" size="md">
-          <Text color="white">Iniciar sesión</Text>
-        </Button>
+//       <Box as="form" onSubmit={handleSubmit} p="20px">
+//         <Input
+//           name="email"
+//           placeholder="Correo electrónico"
+//           value={values.email}
+//           onChange={handleChange}
+//           // error={funcIsError(errors.email || errors.password, touched.email)}
+//           // helperText={funcIsTextError(errors.email, touched.email)}
+//           mb="20px"
+//         />
+//         <Input
+//           name="password"
+//           type="password"
+//           placeholder="Contraseña"
+//           value={values.password}
+//           onChange={handleChange}
+//           // error={funcIsError(errors.password, touched.password)}
+//           // helperText={funcIsTextError(errors.password, touched.password)}
+//           mb="20px"
+//         />
+//         <Button type="submit" size="md">
+//           <Text color="white">Iniciar sesión</Text>
+//         </Button>
 
-        {/* <div >
-          <Link href="/crear-cuenta">Crear cuenta</Link>
-        </div> */}
-      </Box>
-    </Card>
-  )
-}
+//         {/* <div >
+//           <Link href="/crear-cuenta">Crear cuenta</Link>
+//         </div> */}
+//       </Box>
+//     </Card>
+//   )
+// }
 
-export default LoginForm
+// export default LoginForm
--- src/pages/iniciar-sesion/index.js
+++ src/pages/iniciar-sesion/index.js
@@ -1,13 +1,13 @@
 import { Box } from "@chakra-ui/react"
 import CardLogo from "src/components/Molecules/CardLogo"
-import LoginForm from "src/components/Organisms/LoginForm"
+// import LoginForm from "src/components/Organisms/LoginForm"
 
 const LoginPage = () => {
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
       <CardLogo />
 
-      <LoginForm />
+      {/* <LoginForm /> */}
     </Box>
   )
 }
